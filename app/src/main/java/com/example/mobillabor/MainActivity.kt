1cce7a9628501f70813c1ce8b0d827954bb60a0d
Fehér Alíz <feher.aliz@autsoft.hu>
2023-05-30 15:17:02 +0200

    Crashlytics

--- app/src/main/java/com/example/mobillabor/MainActivity.kt
+++ app/src/main/java/com/example/mobillabor/MainActivity.kt
@@ -2,12 +2,25 @@ package com.example.mobillabor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Creates a button that mimics a crash when pressed
+        val crashButton = Button(this)
+        crashButton.text = "Test Crash"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT))
     }
 }
